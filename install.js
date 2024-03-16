a500773e1cf1ad62620b1f5ee7a96f6ba0d60faa
cocktailpeanut <cocktailpeanuts@proton.me>
2024-03-15 20:20:36 -0400

    update

--- install.js
+++ install.js
@@ -39,6 +39,8 @@ module.exports = async (kernel) => {
         path: "app",
         message: [
           "{{gpu === 'nvidia' ? 'conda install -y nvidia/label/cuda-12.1.0::cuda' : null}}",
+          "{{gpu === 'nvidia' && platform === 'win32' ? 'conda install -y cudnn libzlib-wapi -c conda-forge' : null}}",
+          "{{gpu === 'nvidia' && platform === 'linux' ? 'conda install -y cudnn -c conda-forge' : null}}",
           torch_xformers,
           "pip install -r ../requirements.txt"
         ],
