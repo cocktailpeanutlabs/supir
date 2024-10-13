a5953c65f67a6d17b8cb0746754749d40d6b1236
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2024-10-13 22:28:41 +0200

    torch 2.3.1
    Speed up installation

--- pre.js
+++ pre.js
@@ -1,15 +1,15 @@
 module.exports = (config, kernel) => {
   const x = {
     "win32": {
-      "nvidia": `pip install torch torchvision torchaudio ${config.xformers ? 'xformers' : ''} --index-url https://download.pytorch.org/whl/cu121`,
+      "nvidia": `pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 ${config.xformers ? 'xformers' : ''} --index-url https://download.pytorch.org/whl/cu121`,
       "amd": "pip install torch-directml",
-      "cpu": "pip install torch torchvision torchaudio"
+      "cpu": "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cpu"
     },
     "darwin": "pip3 install torch torchvision torchaudio",
     "linux": {
-      "nvidia": `pip install torch torchvision torchaudio ${config.xformers ? 'xformers' : ''}`,
-      "amd": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.7",
-      "cpu": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu"
+      "nvidia": `pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 ${config.xformers ? 'xformers' : ''} --index-url https://download.pytorch.org/whl/cu121`,
+      "amd": "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/rocm6.1",
+      "cpu": "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cpu"
     }
   }
   if (config.torch) {
